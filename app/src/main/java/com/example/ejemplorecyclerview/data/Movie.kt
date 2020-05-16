542eaae919c80c0add9c459febe603664289c198
Enrique Perez <djangomx@MacBook-Pro-de-Enrique.local>
2020-05-16 10:50:06 -0500

    refactor code

--- app/src/main/java/com/example/ejemplorecyclerview/data/Movie.kt
+++ app/src/main/java/com/example/ejemplorecyclerview/data/Movie.kt
@@ -1,28 +1,3 @@
 package com.example.ejemplorecyclerview.data
 
-class Movie(val name: String, val url: String) {
-
-    companion object {
-
-        fun getItems() = listOf(
-            Movie(
-                "Movie 1",
-                "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
-            ),
-            Movie(
-                "Movie 2",
-                "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
-            ),
-            Movie(
-                "Movie 3",
-                "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
-            ),
-            Movie(
-                "Movie 4",
-                "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
-            )
-        )
-
-    }
-
-}
+class Movie(val name: String, val url: String)
